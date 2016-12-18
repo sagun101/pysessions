1e4e8908f8ec732fdd8d71e85627cdbb733993f2
ITSME-PC\ITSME <sagunme80@gmail.com>
2016-12-18 08:36:55 +0545

    no message

---
+++ weatherman/application.py
@@ -0,0 +1,15 @@
+from flask import Flask , render_template
+
+
+
+
+app =Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    print(index())
+    app.run()
